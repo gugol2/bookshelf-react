fc97dcb0cae96f53cb4b5b59e5bf7281cfbf2734
jsc <jsanchezcriado@gmail.com>
2020-03-24 21:10:31 +0100

    turned the Library stateless functional component into a class

--- src/Library.js
+++ src/Library.js
@@ -1,40 +1,46 @@
 import React from 'react';
 import { Bookshelf } from './Bookshelf';
 
-export const Library = (props) => {
-    console.log('books received', props.books);
-    const shelves = props.books.reduce((acc, cur) => {
-        acc[cur.shelf] = [...acc[cur.shelf] || [], cur];
-        return acc
-    }, {});
+export class Library extends React.Component {
 
-    console.log('shelves is:', shelves);
 
-    const moveBook = (book, shelf) => {
+    moveBook = (book, shelf) => {
         console.log('book', book);
         console.log('shelf', shelf);
     }
     
-    return (
-        <div className="list-books">
-            <div className="list-books-title">
-                <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-                <div>
-                    {Object.entries(shelves).map(([key, value]) => (
-                        <Bookshelf 
-                            bookshelfName={key}
-                            booksInThisShelf={value}
-                            key={key}
-                            moveBook={moveBook}
-                        />
-                    ))}
+    render () {
+        const { books, onToggleView } = this.props;
+
+        const shelves = books.reduce((acc, cur) => {
+            acc[cur.shelf] = [...acc[cur.shelf] || [], cur];
+            return acc
+        }, {});
+    
+        console.log('shelves is:', shelves);
+        console.log('books received', books);
+
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        {Object.entries(shelves).map(([key, value]) => (
+                            <Bookshelf 
+                                bookshelfName={key}
+                                booksInThisShelf={value}
+                                key={key}
+                                moveBook={this.moveBook}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="open-search">
+                    <button onClick={onToggleView}>Add a book</button>
                 </div>
             </div>
-            <div className="open-search">
-                <button onClick={props.onToggleView}>Add a book</button>
-            </div>
-        </div>
-    )
+        )
+    }
 }

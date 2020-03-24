f1a48e524c4afde7757e1ab9f23f6265620398c6
jsc <jsanchezcriado@gmail.com>
2020-03-24 21:27:57 +0100

    Revert "turned the Library stateless functional component into a class"
    This reverts commit fc97dcb0cae96f53cb4b5b59e5bf7281cfbf2734.

--- src/Library.js
+++ src/Library.js
@@ -1,46 +1,40 @@
 import React from 'react';
 import { Bookshelf } from './Bookshelf';
 
-export class Library extends React.Component {
+export const Library = (props) => {
+    console.log('books received', props.books);
+    const shelves = props.books.reduce((acc, cur) => {
+        acc[cur.shelf] = [...acc[cur.shelf] || [], cur];
+        return acc
+    }, {});
 
+    console.log('shelves is:', shelves);
 
-    moveBook = (book, shelf) => {
+    const moveBook = (book, shelf) => {
         console.log('book', book);
         console.log('shelf', shelf);
     }
     
-    render () {
-        const { books, onToggleView } = this.props;
-
-        const shelves = books.reduce((acc, cur) => {
-            acc[cur.shelf] = [...acc[cur.shelf] || [], cur];
-            return acc
-        }, {});
-    
-        console.log('shelves is:', shelves);
-        console.log('books received', books);
-
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        {Object.entries(shelves).map(([key, value]) => (
-                            <Bookshelf 
-                                bookshelfName={key}
-                                booksInThisShelf={value}
-                                key={key}
-                                moveBook={this.moveBook}
-                            />
-                        ))}
-                    </div>
-                </div>
-                <div className="open-search">
-                    <button onClick={onToggleView}>Add a book</button>
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    {Object.entries(shelves).map(([key, value]) => (
+                        <Bookshelf 
+                            bookshelfName={key}
+                            booksInThisShelf={value}
+                            key={key}
+                            moveBook={moveBook}
+                        />
+                    ))}
                 </div>
             </div>
-        )
-    }
+            <div className="open-search">
+                <button onClick={props.onToggleView}>Add a book</button>
+            </div>
+        </div>
+    )
 }

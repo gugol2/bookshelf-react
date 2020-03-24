8d40c1de826418bf5f7056184d43b573957496ee
jsc <jsanchezcriado@gmail.com>
2020-03-24 14:34:58 +0100

    created Book Component

---
+++ src/Book.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const Book = (props) => {
+    const { title, authors, imageLinks } = props;
+
+    return (
+        <div className="book">
+            <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
+            <div className="book-shelf-changer">
+                <select>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+                </select>
+            </div>
+            </div>
+            <div className="book-title">{title}</div>
+            <div className="book-authors">{authors[0]}</div>
+        </div>
+    )
+}

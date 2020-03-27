293305da280b9862c7ef0fbb3d6f01efe39975c3
jsc <jsanchezcriado@gmail.com>
2020-03-27 22:53:37 +0100

    Updates

---
+++ precache-manifest.9686712b1d22d6db66e7b0e8d166c2aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/bookshelf-react/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/bookshelf-react/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/bookshelf-react/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "50716658406f4b4b64d0",
+    "url": "/bookshelf-react/static/js/runtime~main.50716658.js"
+  },
+  {
+    "revision": "8a939cd6d01092fcb935",
+    "url": "/bookshelf-react/static/js/main.8a939cd6.chunk.js"
+  },
+  {
+    "revision": "399139c1910aa747bb8e",
+    "url": "/bookshelf-react/static/js/1.399139c1.chunk.js"
+  },
+  {
+    "revision": "8a939cd6d01092fcb935",
+    "url": "/bookshelf-react/static/css/main.60896406.chunk.css"
+  },
+  {
+    "revision": "1525c766fbbdb90883705ff231827847",
+    "url": "/bookshelf-react/index.html"
+  }
+];

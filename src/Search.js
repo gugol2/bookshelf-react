7943369063d161d5d095ae134e9afd8d8244aab1
jsc <jsanchezcriado@gmail.com>
2020-03-27 20:24:44 +0100

    updated comments

--- src/Search.js
+++ src/Search.js
@@ -84,7 +84,7 @@ export class Search extends React.Component {
                         {/*
                             NOTES: The search from BooksAPI is limited to a particular set of search terms.
                             You can find these search terms here:
-                            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                            https://github.com/gugol2/bookshelf-react/blob/master/SEARCH_TERMS.md
     
                             However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                             you don't find a specific author or title. Every search is limited by search terms.

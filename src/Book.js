2513cd9ca610b7921f539338b50a2dad0b86dbb9
jsc <jsanchezcriado@gmail.com>
2020-03-30 01:11:52 +0200

    save rerenders of Book by making it a Pure Component

--- src/Book.js
+++ src/Book.js
@@ -1,35 +1,39 @@
 import React from 'react';
 
-export const Book = (props) => {
-    const { book, moveBook } = props;
-    const { title, authors, imageLinks } = book;
-    const shelf = book.shelf || 'none';
+export class Book extends React.PureComponent {
 
-    const changeShelf = (event) => {
+    changeShelf = (event) => {
         const newShelf = event.target.value;
-
-        moveBook(props.book, newShelf);
+        
+        this.props.moveBook(this.props.book, newShelf);
     }
+    
+    render () {
+        const { book } = this.props;
+        const { title, authors, imageLinks } = book;
+        const shelf = book.shelf || 'none';
+        console.log('rendered book', title);
 
-    return (
-        <div className="book">
-            <div className="book-top">
-            <div 
-                className="book-cover" 
-                style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}>
+        return (
+            <div className="book">
+                <div className="book-top">
+                <div 
+                    className="book-cover" 
+                    style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}>
+                </div>
+                <div className="book-shelf-changer">
+                    <select value={shelf} onChange={this.changeShelf} >
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
+                </div>
+                <div className="book-title">{title}</div>
+                <div className="book-authors">{authors && authors.join(', ')}</div>
             </div>
-            <div className="book-shelf-changer">
-                <select value={shelf} onChange={changeShelf} >
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                </select>
-            </div>
-            </div>
-            <div className="book-title">{title}</div>
-            <div className="book-authors">{authors && authors.join(', ')}</div>
-        </div>
-    )
+        )
+    }
 }
